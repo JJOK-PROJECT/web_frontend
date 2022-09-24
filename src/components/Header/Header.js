d705532c7e8b299ea4c10d6d324a2055cd520f17
B-KJS26 <digyrh456789@gmail.com>
2022-09-24 23:23:35 +0900

    UPDATE | 모달창에 로그인 기능추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import './Header.scss';
-export default function header(){
-    return(
+import Login from '../Login/Lom';
+import { Link } from 'react-router-dom';
+export default function header() {
+    return (
         <body className='header_body'>
             <div className='Header_t'>
                 <div className='header-total'>
                     <div className='header_total'>
                         <div className="header_tit">
                             <div className="logo">
-                                <img src='icon/logo.png' />
+                                <Link to='/'>
+                                    <img src='icon/logo.png' />
+                                </Link>
                             </div>
-
                             <div className="header_button">
                                 <div className="login_button" id="headerbutton">
                                     <button>
-                                        로그인
+                                        <Login />
                                     </button>
                                 </div>
                                 <div className="signup_button" id="headerbutton">
@@ -22,7 +25,7 @@ export default function header(){
                                     </button>
                                 </div>
                             </div>
-                        </div>    
+                        </div>
                     </div>
                 </div>
             </div>
