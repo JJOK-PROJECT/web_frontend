338e5ea3060da981e693d820fde5f9267896ce12
KJS <80664816+B-KJS26@users.noreply.github.com>
2022-09-28 08:48:59 +0900

    UPDATE | Textfield -> input 변경

--- src/components/Login/Lom.js
+++ src/components/Login/Lom.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { TextField } from "@material-ui/core";
 import { Link, useNavigate } from 'react-router-dom';
 import Modal from './Loginmodal';
 import './Lom.scss';
