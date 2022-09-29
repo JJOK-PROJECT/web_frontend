0c5c6b484f7e277dc6a117b61fdd39df2bdd590c
KJS <digyrh456789@gmail.com>
2022-09-29 19:50:13 +0900

    ADD | 회원가입 완성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Main from './page/Main/Main';
 import Header from './components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from "./components/Signup/Signup";
 function App() {
   return (
     <BrowserRouter>
       <Header />
         <Routes>
           <Route path='/' element={<Main />} />
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
     </BrowserRouter>
   );
