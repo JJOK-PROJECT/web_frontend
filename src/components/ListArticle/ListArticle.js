13f3454a4ef46697c1a63a2572da5f3e3fd8b9ee
sookyoungwoo <sookyoung2005@gmail.com>
2022-10-11 22:35:21 +0900

    Chore | 파일 구조 수정

--- src/components/ListArticle/ListArticle.js
+++ src/components/ListArticle/ListArticle.js
@@ -1,34 +1,27 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./ListArticle.scss";
-export default function ListArticle(props){
-    return(
-        <>
-        <div className="listarticle-tit">
-            <div className="listarticle-tit-thumbnail">
-                <div className="list-article-tit-div">   
-                <a className="list-article-a"> 
-                    <Link to={`/welbato/${props.link}`}>
-                        <div className="list-article-tit-div-front">
-                            <div className="list-article-div-img">
-                                <img className="list-article-image" src={props.image} alt="product"/>
+export default function ListArticle(props) {
+    return (
+        <div className="listarticle-body">
+                <div className="Listarticle-main-div">
+                    <div className="listarticle-tit-div">
+                        <div className="listarticle-tit-thumbnail">
+                            <div className="list-article-tit-div">
+                                <Link to={`/welbato/${props.link}`}>
+                                    <div className="list-article-div-img">
+                                        <img className="list-article-image" src={props.image} alt="product" />
+                                    </div>
+                                </Link>
                             </div>
+
                             <div className="list-article-div-name">
                                 <span className="list-article-name">
                                     {props.name}
                                 </span>
-                             </div>
-                        </div>
-                        <div className="list-article-tit-div-back">
-                            <div className="list-article-div-message">
-                                    <span className="list-article-nessage">
-                                        {props.message}
-                                    </span>
                             </div>
-                        </div>    
-                    </Link></a>
-                </div>
-            </div>
+                        </div>
+                    </div>
+                </div>    
         </div>
-        </>
     )
 }
--- src/page/Main/Main.js
+++ src/page/Main/Main.js
@@ -3,9 +3,8 @@ import BannerData from "./banner.json";
 import ListData from "./list.json";
 import Listcomponent from "../../components/ListArticle/ListArticle";
 import Banner from "../../components/Banner/Banner";
-import { SwiperSlide,Swiper } from 'swiper/react';
 import {Link} from "react-router-dom";
-import "swiper/scss";
+
 export default function Main(){
     
     return(
@@ -14,24 +13,17 @@ export default function Main(){
                 banner={BannerData.banners}
             />
             <div className='main-category-swiper'>
-                <Swiper
-                    spaceBetween={220}
-                    slidesPerView={7}
-                    loop={true}
-                >
-                {
+               
+            {
                     ListData.list.map(li => (
-                        <SwiperSlide>
-                            <Listcomponent
-                                image={li.image}
-                                name={li.name}
-                                message={li.message}
-                                link={li.link}
-                            />
-                        </SwiperSlide>
+                        <Listcomponent
+                            image={li.image}
+                            name={li.name}
+                            message={li.message}
+                            link={li.link}
+                        />
                     ))
                 }
-                </Swiper>
             </div>
         </section>
     )
