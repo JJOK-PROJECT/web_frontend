811458e77cdef7bb13ebb7a86483b5c95e61bd3a
sookyoungwoo <sookyoung2005@gmail.com>
2022-10-13 17:50:45 +0900

    Chore | 메인화면 총 수정

--- src/components/ListArticle/ListArticle.js
+++ src/components/ListArticle/ListArticle.js
@@ -1,27 +1,22 @@
 import { Link } from "react-router-dom";
 import "./ListArticle.scss";
-export default function ListArticle(props) {
+import ListData from "./list.json";
+import ArticleData from "../ArticleData/ArticleData";
+export default function ListArticle() {
     return (
         <div className="listarticle-body">
-                <div className="Listarticle-main-div">
-                    <div className="listarticle-tit-div">
-                        <div className="listarticle-tit-thumbnail">
-                            <div className="list-article-tit-div">
-                                <Link to={`/welbato/${props.link}`}>
-                                    <div className="list-article-div-img">
-                                        <img className="list-article-image" src={props.image} alt="product" />
-                                    </div>
-                                </Link>
-                            </div>
-
-                            <div className="list-article-div-name">
-                                <span className="list-article-name">
-                                    {props.name}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>    
+            <ul className="Listarticle-main-div">
+                {
+                    ListData.list.map(li => (
+                        <ArticleData
+                            image={li.image}
+                            name={li.name}
+                            message={li.message}
+                            link={li.link}
+                        />
+                    ))
+                }
+            </ul>    
         </div>
     )
 }
