2de1ac982841d18d378685cfe8bc1d196aa326b1
sookyoungwoo <sookyoung2005@gmail.com>
2022-11-16 17:55:34 +0900

    ADD :: Login axios 연결

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,5 +1,5 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const customAxios = axios.create({
-//     baseURL: "http://13.125.225.199:8003"
-// })
+export const customAxios = axios.create({
+    baseURL: "http://13.125.225.199:8003"
+})
